e6e03bb4efe967a53d5756b7f9a6b22eca8f32a7
sawe-daisy <deesawe16@gmail.com>
2021-01-03 14:24:26 +0300

    registring models

--- findwork/admin.py
+++ findwork/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User,Job,Application
+from django.contrib import admin
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Job)
+admin.site.register(Application)
